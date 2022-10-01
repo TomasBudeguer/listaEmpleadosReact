81f02936d35fb2871d42cb84d8254525d1fc5b07
Tomas <tomibudeguer20@gmail.com>
2022-10-01 13:14:15 -0300

    cambie las clases row por el componente de bootstrap y cambie el read me

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Lista de empleados
+
+## Autor:
+Tomas Agustin Budeguer
+
+## Librerias usadas
+
+1. React-Bootstrap
+1. Bootstrap
--- src/components/EmpleadoRow.jsx
+++ src/components/EmpleadoRow.jsx
@@ -1,24 +1,27 @@
 import EmpleadoAvatar from "./EmpleadoAvatar";
-import {Badge} from "react-bootstrap"
+import { Badge, Col, Row } from "react-bootstrap";
+
 const EmpleadoRow = ({ arregloEmpleado }) => {
   return (
     <div>
       {arregloEmpleado.map((empleado, id) => (
-        <div className="row" key={id}>
-          <aside className="col-2">
+        <Row key={id}>
+          <Col sm={12} md={4} lg={2}>
             <EmpleadoAvatar empleado={empleado}></EmpleadoAvatar>
-          </aside>
-          <aside className="col-10">
+          </Col>
+          <Col sm={12} md={8} lg={4} className='text-center'>
             <div>
-              <h3>{empleado.fullName}</h3>
-              <p>
+              <h3 className="mt-3">{empleado.fullName}</h3>
+              <p className="mb-3">
                 {empleado.title}
-                <Badge bg="info" className="ms-4">{empleado.department}</Badge>
+                <Badge bg="info" className="ms-4">
+                  {empleado.department}
+                </Badge>
               </p>
             </div>
-          </aside>
+          </Col>
           <hr />
-        </div>
+        </Row>
       ))}
     </div>
   );
